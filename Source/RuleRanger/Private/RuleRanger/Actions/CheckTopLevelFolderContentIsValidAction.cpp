3bffe8db3510f7d2a171ccacf39a34d26ef1ade7
Peter Donald <peter@realityforge.org>
2023-12-27 23:53:11 +1100

    Simplify code when emitting errors

--- Source/RuleRanger/Private/RuleRanger/Actions/CheckTopLevelFolderContentIsValidAction.cpp
+++ Source/RuleRanger/Private/RuleRanger/Actions/CheckTopLevelFolderContentIsValidAction.cpp
@@ -51,14 +51,7 @@ void UCheckTopLevelFolderContentIsValidAction::Apply_Implementation(URuleRangerA
                             ? TEXT("")
                             : *FString::Printf(TEXT(" or the asset name must match regex %s"), *ValidAssetRegexPattern))
                     : Message;
-                if (Message.IsEmpty())
-                {
-                    ActionContext->Error(FText::FromString(ErrorMessage));
-                }
-                else
-                {
-                    ActionContext->Error(FText::FromString(Message));
-                }
+                ActionContext->Error(FText::FromString(ErrorMessage));
             }
         }
         else
@@ -76,14 +69,7 @@ void UCheckTopLevelFolderContentIsValidAction::Apply_Implementation(URuleRangerA
                                           : *FString::Printf(TEXT(" or the folder name must match regex %s"),
                                                              *ValidFolderRegexPattern))
                     : Message;
-                if (Message.IsEmpty())
-                {
-                    ActionContext->Error(FText::FromString(ErrorMessage));
-                }
-                else
-                {
-                    ActionContext->Error(FText::FromString(Message));
-                }
+                ActionContext->Error(FText::FromString(ErrorMessage));
             }
         }
     }
